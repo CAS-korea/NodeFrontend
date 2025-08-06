39109ce3418cf7fffa9d3146a4363bd5ee3633cb
sejin <draveos20@gmail.com>
2025-08-06 09:43:06 +0900

    Changed to Published Backend

--- src/constants/ServerUrl.ts
+++ src/constants/ServerUrl.ts
@@ -1,3 +1,7 @@
-export enum ServerUrl {
-    SERVER = 'http://localhost:8080'
+export enum ServerUrl{
+    SERVER = 'https://nodebackend-z1qs.onrender.com/'
 }
+
+//export enum ServerUrl {
+  //  SERVER = 'http://localhost:8080'
+//}
